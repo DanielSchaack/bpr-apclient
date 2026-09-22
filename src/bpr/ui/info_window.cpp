5eca4260c980e168cc452628d0335dbc1bfa4b29
FyreDay <fyredaymc@gmail.com>
2026-09-22 15:12:56 -0500

    change width

--- src/bpr/ui/info_window.cpp
+++ src/bpr/ui/info_window.cpp
@@ -21,6 +21,7 @@ void InfoWindow::Draw(int outerWidth, int outerHeight, float uiScale) {
     ImGui::TextUnformatted("AP Client Temp Logger. Press F3 to toggle");
     ImGui::TextUnformatted("");
     for (const auto& msg : logMessages) {
+        ImGui::SetNextItemWidth(-1.0f); 
         ImGui::InputText("##Text", (char*)msg.c_str(), msg.length(), ImGuiInputTextFlags_ReadOnly);
     }
 
